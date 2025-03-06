df0414bb6365a4775feef195e5118e476a371444
diegratia <d.febrian2002@gmail.com>
2025-03-06 15:25:32 +0700

    feat: add Dockerfile and compose

--- TrackingBle/obj/Debug/net8.0/TrackingBle.AssemblyInfo.cs
+++ TrackingBle/obj/Debug/net8.0/TrackingBle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrackingBle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f64c80e213ae24f870847e4902d295c3fb7c9dca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3275b0ff598ee3791a19df9bde53fc5fc694d8b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrackingBle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrackingBle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
